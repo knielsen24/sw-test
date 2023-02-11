5b22d2ec9a00b90f635fa2d60a8da0f9111ac466
Kevin Nielsen <knielsen24@gmail.com>
2023-02-11 10:59:49 -0800

    map margins

--- client/src/Section-About/PropertyMap.js
+++ client/src/Section-About/PropertyMap.js
@@ -9,7 +9,7 @@ function PropertyMap() {
         "https://res.cloudinary.com/dooa4lvvv/image/upload/v1676007462/Southwells/assets/300_S_Wells_ave_MAP_landmarks-med_uq0sji.png";
 
     return (
-        <div className="row  align-items-center p-5 ">
+        <div className="row align-items-start justify-content-evenly p-5 ">
             <div className="my-4">
                 <h4 className="p-2 fw-bold text-uppercase">
                     Strategically located in a dense and established residential
@@ -18,29 +18,27 @@ function PropertyMap() {
                 <p>Amenities within 3-miles of Radius</p>
             </div>
 
-            <div className="col list-width">
-                <div className="row justify-content-center my-3">
-                    <div className="col ">
-                        <img
-                            className="rounded-4 highlight-image-width"
-                            src={mapURL}
-                            alt="map location"
-                            width="300"
-                        />
-                    </div>
+            <div className="col float-end">
+                <div className="m-4">
+                    <img
+                        className="rounded-4 border highlight-image-width"
+                        src={mapURL}
+                        alt="map location"
+                        // width="300"
+                    />
                 </div>
-                <div className="row mt-5 justify-content-center">
-                    <div className="col">
-                        <a
-                            className="btn btn-primary btn-sm text-decoration-none shadow-sm text-uppercase"
-                            target="_blank"
-                            href={googleMaps}
-                            rel="noreferrer"
-                        >
-                            view on Google Maps
-                        </a>
-                    </div>
-                </div>
+                <a
+                    className="btn btn-primary btn-sm text-decoration-none shadow-sm text-uppercase"
+                    target="_blank"
+                    href={googleMaps}
+                    rel="noreferrer"
+                >
+                    view on Google Maps
+                </a>
+                <div className="row my-3  "></div>
+                {/* <div className="row mt-5 ">
+                    <div className="col"></div>
+                </div> */}
             </div>
             <div className="col me-5">
                 <div className="row my-3">
