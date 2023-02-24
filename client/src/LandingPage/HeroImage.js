ccb6352b084e832e1e93a724f27d598f78f77edf
Kevin Nielsen <knielsen24@gmail.com>
2023-02-23 20:03:55 -0800

    add css to hero header

--- client/src/LandingPage/HeroImage.js
+++ client/src/LandingPage/HeroImage.js
@@ -3,11 +3,12 @@ import React from "react";
 function HeroImage() {
     return (
         <div className="row hero-container justify-content-center align-items-start">
-            <div className="col hero-header mb-4 p-5">
-                <h2 className="mt-2 mx-auto px-5 p-4 bg-white bg-opacity-75 rounded">
+            <div className="col hero-header pt-3">
+                <h1 className="display-5 mt-2 mx-auto px-5 py-4 fw-bold rounded text-white">
+                {/* bg-white bg-opacity-75 */}
                     Comercial Real Estate Property
                     <h4 className="pt-3 text-muted">Near Downtown Reno</h4>
-                </h2>
+                </h1>
             </div>
         </div>
     );
